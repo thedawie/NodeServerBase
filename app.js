9cec7fc3348b4d0450ebc4e1cfd958d37bdc8edb
David <thedawie@gmail.com>
2016-02-18 10:12:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function(err, req, res, next) {
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('app listening on port 3000!');
 });
 
 module.exports = app;
